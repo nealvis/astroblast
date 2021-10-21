51b91947b6e019d1f3284900e1cc0b404f621e9f
Neal Smith <nealvis@gmail.com>
2021-10-21 07:03:09 -0700

    added batch files to build and run the program

---
+++ build.bat
@@ -0,0 +1,5 @@
+REM need the KICK_ASS_JAR_DIR environment variable set to something like
+"C:\...<dirs here>...\kick_assembler"
+
+java -jar %KICKASS_JAR_DIR%\KickAss.jar ./astroblast.asm
+
---
+++ run.bat
@@ -0,0 +1,3 @@
+REM need something like this in the path: C:\....\GTK3VICE-3.5-win64\bin\
+
+x64sc.exe astroblast.prg
